9c5b85f6c3c0b3622a578910c26bebcc1e6d8bdf
Saleh Albatati <0xack13@gmail.com>
2015-01-22 12:34:45 +0300

    Add simple route

--- hello.rb
+++ hello.rb
@@ -2,4 +2,10 @@ require 'sinatra'
 
 get '/hi' do
   "Hello World!"
-  end
+end
+
+get '/hello/:name' do
+    # matches "GET /hello/foo" and "GET /hello/bar"
+    # params[:name] is 'foo' or 'bar'
+    "Hello #{params[:name]}!"
+end
