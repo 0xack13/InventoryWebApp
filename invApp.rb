e70b61ab70dc59760f9fbc15daebba2c6cb8d435
Saleh Albatati <0xack13@gmail.com>
2015-03-15 20:49:24 +0300

    List users

--- invApp.rb
+++ invApp.rb
@@ -821,7 +821,7 @@ if(e.which == 17) isCtrl=false;
   <li class="nav-item"><a href="/upload" id="upload"><span class="glyphicon glyphicon-upload"></span>&nbsp;Upload</a></li>
   <li class="nav-item"><a href="/media"><span class="glyphicon glyphicon-folder-open"></span>&nbsp;Media</a></li>
   <li class="nav-item"><a href="/dashboard"><span class="glyphicon glyphicon-dashboard"></span>&nbsp;Dashboard</a></li>
-  <li class="nav-item"><a href="/addUser"><span class="glyphicon glyphicon-user"></span>&nbsp;Users</a></li>
+  <li class="nav-item"><a href="/listUsers"><span class="glyphicon glyphicon-user"></span>&nbsp;Users</a></li>
   <li class="nav-item"><a href="mailto:support@support.com"><span class="glyphicon glyphicon-question-sign"></span>&nbsp;Help</a></li>
   <li class="nav-item"><a href="/logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Log out</a></li>
 </ul>
